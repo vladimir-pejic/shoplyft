83e432096dfbb9c846c04c05c7582345e0fd89c7
vladimir_kompitenz <v.pejic@kompitenz.de>
2020-10-16 13:33:37 +0200

    Router & Products

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap";
 
 const Header = () => {
     return (
         <header>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="/">Shoplyft</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>Shoplyft</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
                         <Nav className="mr-auto">
                             <NavDropdown title="Categories" id="collasible-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Item>Action</NavDropdown.Item>
+                                <NavDropdown.Item>Another action</NavDropdown.Item>
+                                <NavDropdown.Item>Something</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                <NavDropdown.Item>Separated link</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                         <Nav className="ml-auto">
-                            <Nav.Link href="#features"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
-                            <Nav.Link href="#pricing"><i className="fas fa-user"></i> Sign In</Nav.Link>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/signin'>
+                                <Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Product from "../components/Product";
+import {Link} from "react-router-dom";
+import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from "react-bootstrap";
+import Rating from "../components/Rating";
+import products from "../products";
+
+const ProductScreen = ({ match }) => {
+    const product = products.find(p => p._id === match.params.id);
+    console.log(product);
+
+    return (
+        <>
+            <Link className='btn btn-light my-3' to='/'>Go back</Link>
+
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroupItem>
+                            <h2>{product.name}</h2>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Rating text={`${product.numReviews} reviews`} value={product.rating} />
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            Price: ${product.price}
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            {product.description}
+                        </ListGroupItem>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant="flush">
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>
+                                        Price:
+                                    </Col>
+                                    <Col>
+                                        <strong>${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>
+                                        Status:
+                                    </Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
+                                    Add to Cart
+                                </Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </>
+    );
+};
+
+export default ProductScreen;
