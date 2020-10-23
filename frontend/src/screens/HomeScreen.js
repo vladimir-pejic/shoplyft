55875bfe6053933f6017d8a47f90e5dcc553bc66
vladimir_kompitenz <v.pejic@kompitenz.de>
2020-10-23 22:41:42 +0200

    Loader & Messages

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -2,9 +2,9 @@ import React, {useEffect} from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import {Row, Col} from "react-bootstrap";
 import Product from "../components/Product";
-import axios from 'axios';
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 import {listProducts} from "../actions/productActions.js";
-import {load} from "dotenv";
 
 const HomeScreen = () => {
     const dispatch = useDispatch();
@@ -20,9 +20,9 @@ const HomeScreen = () => {
         <>
             <h1>Latest Products</h1>
             { loading ? (
-                <h2>Loading...</h2>
+                <Loader />
             ) : error ? (
-                <h3>{ error }</h3>)
+                <Message variant='danger'>{ error }</Message> )
             : (
                 <Row>
                     {products.map(product => (
